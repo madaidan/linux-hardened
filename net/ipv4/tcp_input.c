7ee82e5e68f98d5e2451450204338f2d20052056
madaidan <50278627+madaidan@users.noreply.github.com>
2020-02-09 00:03:41 +0000

    Disable TCP simultaneous connect
    This is modified from Brad Spengler/PaX Team's code in the last
    public patch of grsecurity/PaX based on my understanding of the code. Changes
    or omissions from the original code are mine and don't reflect the original
    grsecurity/PaX code.
    
    TCP simultaneous connect adds a weakness in Linux's
    implementation of TCP that allows two clients to connect
    to each other without either entering a listening state
    The weakness allows an attacker to easily prevent a client from
    connecting to a known server provided the source port for
    the connection is guessed correctly.
    
    As the weakness could be used to prevent an antivirus or IPS from
    fetching updates, or prevent an SSL gateway from fetching a CRL,
    it should be eliminated.
    
    This creates a net.ipv4.tcp_simult_connect sysctl that when
    disabled, disables TCP simultaneous connect. The default is
    to disable it.

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -82,6 +82,7 @@
 #include <net/mptcp.h>
 
 int sysctl_tcp_max_orphans __read_mostly = NR_FILE;
+int sysctl_tcp_simult_connect __read_mostly = 0;
 
 #define FLAG_DATA		0x01 /* Incoming frame contained data.		*/
 #define FLAG_WIN_UPDATE		0x02 /* Incoming ACK was a window update.	*/
@@ -316,7 +317,7 @@ static void tcp_ecn_rcv_synack(struct tcp_sock *tp, const struct tcphdr *th)
 
 static void tcp_ecn_rcv_syn(struct tcp_sock *tp, const struct tcphdr *th)
 {
-	if ((tp->ecn_flags & TCP_ECN_OK) && (!th->ece || !th->cwr))
+	if ((tp->ecn_flags & TCP_ECN_OK) && (!th->ece || !th->cwr) && sysctl_tcp_simult_connect)
 		tp->ecn_flags &= ~TCP_ECN_OK;
 }
 
@@ -6054,7 +6055,7 @@ discard:
 	    tcp_paws_reject(&tp->rx_opt, 0))
 		goto discard_and_undo;
 
-	if (th->syn) {
+	if (th->syn && sysctl_tcp_simult_connect) {
 		/* We see SYN without ACK. It is attempt of
 		 * simultaneous connect with crossed SYNs.
 		 * Particularly, it can be connect to self.
