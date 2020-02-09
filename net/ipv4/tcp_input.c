c129b3e1e41dbc0442397d7176e78aa109eda2cc
madaidan <50278627+madaidan@users.noreply.github.com>
2020-02-09 15:26:12 +0000

    Remove unneeded check

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -317,7 +317,7 @@ static void tcp_ecn_rcv_synack(struct tcp_sock *tp, const struct tcphdr *th)
 
 static void tcp_ecn_rcv_syn(struct tcp_sock *tp, const struct tcphdr *th)
 {
-	if ((tp->ecn_flags & TCP_ECN_OK) && (!th->ece || !th->cwr) && sysctl_tcp_simult_connect)
+	if ((tp->ecn_flags & TCP_ECN_OK) && (!th->ece || !th->cwr))
 		tp->ecn_flags &= ~TCP_ECN_OK;
 }
 
